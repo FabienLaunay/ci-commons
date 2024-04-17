7ab9b3e7198d131ce344977616bd8a6f23017246
fabien.launay <fabien.launay.email@gmail.com>
2024-04-17 16:35:02 +0900

    A

--- cfg/gitlint/rules/TitleImperativeMood.py
+++ cfg/gitlint/rules/TitleImperativeMood.py
@@ -1,3 +1,10 @@
+#  This material contains trade secrets or otherwise confidential information owned by
+#  Siemens Industry Software Inc. or its affiliates (collectively, "SISW"), or its licensors.
+#  Access to and use of this information is strictly limited as set forth in the Customer's
+#  applicable agreements with SISW.
+#
+#  Copyright 2024 Siemens
+
 from gitlint.rules import LineRule, RuleViolation, CommitMessageTitle
 
 class TitleImperativeMood(LineRule):
@@ -8,7 +15,7 @@ class TitleImperativeMood(LineRule):
     def validate(self, line, commit):
         words = line.split(': ', 1)[-1].split()
         first_word = words[0].lower()
-        verbs = open('cfg/gitlint/gitlintUserDefinedRules/Verbs.txt', 'r')
+        verbs = open('cfg/gitlint/gitlint/txt/Verbs.txt', 'r')
         verbsList = verbs.read().splitlines()
         verbs.close()
         found = False
