f9c51ed40f622e8a44e7f083c1a832c87b11f256
Fabien Launay <fabien.launay.email@gmail.com>
2024-10-15 23:54:27 +0900

    FEA: Update README.md

--- bsh/lintCiCommonsProjectBeforeScript.bash
+++ bsh/lintCiCommonsProjectBeforeScript.bash
@@ -1,17 +1,65 @@
 #!/bin/bash
 
-echo "###################################################################"
-echo "Update Debian-based Gitlab Linux runner local package index."
-echo "###################################################################"
-echo ""
-apt-get update
-echo "###################################################################"
-echo "Install the 'git' package (prerequisite for gitlint)."
-echo "###################################################################"
-echo ""
-apt-get install --yes git
-echo "###################################################################"
-echo "Install the 'gitlint' package using Python package installer."
-echo "###################################################################"
-echo ""
-pip install gitlint
+. bsh/utilities/message.bash
+
+printLargetTextBox "Git Commit Messages Linter"
+
+currentL1TaskNumber=1
+totalL1TaskCount=5
+
+# ==============================================================================
+# Update Debian-based Gitlab Linux runner local package index.
+# ==============================================================================
+
+# Print starting text box level 1.
+message="Gitlab runner local package index update"
+printStartingL1TaskTextBox "$message"
+currentLvlOneTaskStartTimeStamp=$(date +%s)
+
+#Print command and run it.
+command="apt-get update"
+printExecutingCommand "$command"
+$command
+
+# Print completed text box level 1.
+currentLvlOneTaskEndTimeStamp=$(date +%s)
+printCompletedL1TaskTextBoxAndIncrementCounter "$message" \
+  "$((currentLvlOneTaskEndTimeStamp - currentLvlOneTaskStartTimeStamp))"
+
+# ==============================================================================
+# Install the 'git' package (prerequisite for gitlint)
+# ==============================================================================
+
+# Print starting text box level 1.
+message="'Git' Debian package installation"
+printStartingL1TaskTextBox "$message"
+currentLvlOneTaskStartTimeStamp=$(date +%s)
+
+#Print command and run it.
+command="apt-get install --yes git"
+printExecutingCommand "$command"
+$command
+
+# Print completed text box level 1.
+currentLvlOneTaskEndTimeStamp=$(date +%s)
+printCompletedL1TaskTextBoxAndIncrementCounter "$message" \
+  "$((currentLvlOneTaskEndTimeStamp - currentLvlOneTaskStartTimeStamp))"
+
+# ==============================================================================
+# Install the 'gitlint' package using Python package installer
+# ==============================================================================
+
+# Print starting text box level 1.
+message="'gitlint' Python package installation"
+printStartingL1TaskTextBox "$message"
+currentLvlOneTaskStartTimeStamp=$(date +%s)
+
+#Print command and run it.
+command="pip install gitlint"
+printExecutingCommand "$command"
+$command
+
+# Print completed text box level 1.
+currentLvlOneTaskEndTimeStamp=$(date +%s)
+printCompletedL1TaskTextBoxAndIncrementCounter "$message" \
+  "$((currentLvlOneTaskEndTimeStamp - currentLvlOneTaskStartTimeStamp))"
---
+++ bsh/utilities/message.bash
@@ -0,0 +1,523 @@
+#!/bin/bash
+
+# #############################################################################
+# Copyright (C) 2023-2024 Fabien Launay. All Rights Reserved.
+# Author: Fabien Launay.
+# Email : fabien.launay.email@gmail.com.
+# Source: This file is part of the "Reactor Coding Challenge 01" project Git
+#         repository developed by Fabien Launay for Woven by Toyota.
+# Usage : This copyright notice may not be removed from this file.
+# #############################################################################
+
+. bsh/utilities/format.bash
+
+# #############################################################################
+# This bash file contains functions that perform operations related to message
+# printing and prompting.
+#
+# Author: Fabien Launay (fabien.launay.email@gmail.com)
+#
+# #############################################################################
+
+# =============================================================================
+# Print bash script tasks.
+#
+# Prints to the terminal the list of tasks to be executed by a bash script.
+#
+# @param $1 Tasks to be printed to the terminal.
+#
+# @return 0
+# =============================================================================
+
+printBashScriptTasks() {
+	local tasks=$1
+	echo
+	echo "This Bash script will perform the following task(s):"
+	echo -e "$tasks"
+	return 0
+}
+
+# =============================================================================
+# Ask user to continue.
+#
+# Prints to the terminal a message asking the user whether to continue or not.
+# Terminates the script if the user presses a key other than 'y' and 'Y'.
+#
+# @return 0 if user presses the 'y' or 'Y' key.
+# =============================================================================
+
+askUserToContinue() {
+	echo
+	read \
+		-p "Press Y to continue or any other key to cancel: " \
+		-n 1 \
+		-r
+	echo
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		return 0
+	else
+		exit 0
+	fi
+}
+
+# =============================================================================
+# Print border.
+#
+# Prints to the terminal the same character multiple times, with the specified
+# foreground and background colors.
+#
+# Example: The following command:
+#
+#   ------------------------------------
+#   printBorder 30 '~' '\e[31m' '\e[42m'
+#   ------------------------------------
+#
+#   would print the following characters to the terminal (red foreground color,
+#   green background color):
+#
+#   ------------------------------------
+#   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#   ------------------------------------
+#
+# @param $1 Number of times to print the character.
+# @param $2 Character to print.
+# @param $3 ANSI escape sequence code for the character foreground color.
+# @param $4 ANSI escape sequence code for the character background color.
+#
+# @return 0 if user presses the 'y' or 'Y' key.
+# =============================================================================
+
+printBorder() {
+	local width=$1
+	local character=$2
+	local fgColor=$3
+	local bgColor=$4
+
+	# Store the sequence of characters to be printed in the `border` variable.
+	printf -v border "$character%.0s" $(seq 1 $width)
+
+	# Print the characters to the terminal surrounded by the appropriate ANSI
+	# escape sequence codes
+	echo -e $fgColor$bgColor$border$UNSET_STYLE_ALL
+
+	return 0
+}
+
+# =============================================================================
+# Print delimited line.
+#
+# Prints to the terminal a message of specified length, delimited by a
+# character, with the specified foreground and background colors, and
+# optionally centered.
+#
+# Example: The following commands:
+#
+#   ------------------------------------
+#   printDelimitedLine 10 '~' '\e[31m' '\e[42m' 'L' 'This text is not centered'
+#   printDelimitedLine 10 '~' '\e[31m' '\e[42m' 'C' 'This text is centered'
+#   ------------------------------------
+#
+#   would print the following lines to the terminal (red foreground color,
+#   green background color):
+#
+#   ------------------------------------
+#   ~ This text is not centered  ~
+#   ~   This text is centered    ~
+#   ------------------------------------
+#
+# @param $1 Line length.
+# @param $2 Character used to surround the message.
+# @param $3 ANSI escape sequence code for the line foreground color.
+# @param $4 ANSI escape sequence code for the line background color.
+# @param $5 Alignment (centered if value is "C", left-aligned otherwise).
+# @param $6 Message to be printed on the line.
+#
+# @return 0.
+# =============================================================================
+
+printDelimitedLine() {
+	local lineLength=$1
+	local character=$2
+	local fgColor=$3
+	local bgColor=$4
+	local alignment=$5
+	local message=$6
+
+	local messageLength=${#message}
+
+	# Store the sequence of characters to be printed in the `border` variable.
+	if ((messageLength > lineLength)); then
+		printf -v line "$character $message $character"
+	else
+		if [ "$alignment" = "C" ]; then
+			paddingSize=$((lineLength - messageLength))
+			rightPaddingSize=$((paddingSize / 2))
+			leftPaddingSize=$((rightPaddingSize - paddingSize))
+			printf -v line "%*s$message%*s" \
+				$leftPaddingSize $character $rightPaddingSize $character
+		else
+			paddingSize=$((lineLength - messageLength - 2))
+			printf -v line "$character $message%*s" $paddingSize $character
+		fi
+	fi
+
+	# Print the characters to the terminal surrounded by the appropriate ANSI
+	# escape sequence codes
+	echo -e $fgColor$bgColor"$line"$UNSET_STYLE_ALL
+
+	return 0
+}
+
+# =============================================================================
+# Print large text box.
+#
+# Prints to the terminal a message centered in a pre-formatted text box,
+# surrounded by the application and author names.
+#
+# @param $1 Message to be printed in the pre-formatted text box.
+#
+# @return 0.
+# =============================================================================
+
+printLargetTextBox() {
+	message=$1
+	CHARACTER="#"
+	WIDTH=80
+
+	echo
+
+	printBorder $WIDTH $CHARACTER $FG_COLOR_WHITE $BG_COLOR_GREEN
+
+	printDelimitedLine $WIDTH $CHARACTER $FG_COLOR_WHITE $BG_COLOR_GREEN "L" \
+		""
+	printDelimitedLine $WIDTH $CHARACTER $FG_COLOR_WHITE $BG_COLOR_GREEN "C" \
+		"REACTOR CODING CHALLENGE 01 - Version 1.0.0"
+	printDelimitedLine $WIDTH $CHARACTER $FG_COLOR_WHITE $BG_COLOR_GREEN "L" \
+		""
+	printDelimitedLine $WIDTH $CHARACTER $FG_COLOR_WHITE $BG_COLOR_GREEN "C" \
+		"$message"
+	printDelimitedLine $WIDTH $CHARACTER $FG_COLOR_WHITE $BG_COLOR_GREEN "L" \
+		""
+	printDelimitedLine $WIDTH $CHARACTER $FG_COLOR_WHITE $BG_COLOR_GREEN "C" \
+		"Author: Fabien Launay (fabien.launay.email@gmail.com) "
+	printDelimitedLine $WIDTH $CHARACTER $FG_COLOR_WHITE $BG_COLOR_GREEN "L" \
+		""
+
+	printBorder $WIDTH $CHARACTER $FG_COLOR_WHITE $BG_COLOR_GREEN
+
+	return 0
+}
+
+# =============================================================================
+# Print normal text box.
+#
+# Prints to the terminal a message left-aligned in a pre-formatted text box.
+#
+# @param $1 ANSI escape sequence code for the text box foreground color.
+# @param $2 ANSI escape sequence code for the text box background color.
+# @param $3 Character used to make the borders of the text box.
+# @param $4 Message to be printed in the pre-formatted text box.
+#
+# @return 0.
+# =============================================================================
+
+printNormalTextBox() {
+
+	local fgColor=$1
+	local bgColor=$2
+	local character=$3
+	local message=$4
+
+	WIDTH=80
+
+	printBorder $WIDTH $character $fgColor $bgColor
+	printDelimitedLine $WIDTH $character $fgColor $bgColor "L" "$message"
+	printBorder $WIDTH $character $fgColor $bgColor
+	echo
+
+	return 0
+}
+
+# =============================================================================
+# Print staring level 1 task text box.
+#
+# Prints to the terminal a staring task message in a pre-formatted text box
+# whose:
+# - Border is the "=" character
+# - Foreground text color is white.
+# - Background text color is blue.
+#
+# @param $1 Message to be printed in the text box.
+#
+# @return 0.
+# =============================================================================
+
+printStartingL1TaskTextBox() {
+	local message=$1
+
+	echo
+
+	printNormalTextBox \
+		$FG_COLOR_WHITE \
+		$BG_COLOR_BLUE \
+		"=" \
+		"[$currentL1TaskNumber/$totalL1TaskCount] Starting $message..."
+
+	return 0
+}
+
+# =============================================================================
+# Print completed level 1 task text box and increment level 1 task counter.
+#
+# Prints to the terminal a completed task message, including the task execution
+# duration, in a pre-formatted text box whose:
+# - Border is the "=" character
+# - Foreground text color is black.
+# - Background text color is bright blue.
+#
+# @param $1 Message to be printed in the text box.
+# @param $2 Duration of the completed task.
+#
+# @return 0.
+# =============================================================================
+
+printCompletedL1TaskTextBoxAndIncrementCounter() {
+	local message=$1
+	local duration=$2
+
+	echo
+
+	# Store in the  `hhmmss` variable the task execution duration in the
+	# `hh:mm:ss` format.
+	printf -v hhmmss \
+		'%02dh:%02dm:%02ds' \
+		$((duration / 3600)) $((duration % 3600 / 60)) $((duration % 60))
+
+	# Print to the terminal the pre-formatted text box.
+	printNormalTextBox \
+		$FG_COLOR_BLACK \
+		$BG_COLOR_BRIGHT_BLUE \
+		"=" \
+		"[$currentL1TaskNumber/$totalL1TaskCount] Completed $message in $hhmmss"
+	((currentL1TaskNumber++))
+
+	return 0
+}
+
+# =============================================================================
+# Print staring level 2 task text box.
+#
+# Prints to the terminal a staring task message in a pre-formatted text box
+# whose:
+# - Border is the "+" character
+# - Foreground text color is white.
+# - Background text color is yellow.
+#
+# @param $1 Message to be printed in the text box.
+#
+# @return 0.
+# =============================================================================
+
+printStartingL2TaskTextBox() {
+	local message=$1
+
+	echo
+
+	printNormalTextBox \
+		$FG_COLOR_WHITE \
+		$BG_COLOR_YELLOW \
+		"+" \
+		"[$currentL2TaskNumber/$totalL2TaskCount] Starting $message..."
+
+	return 0
+}
+
+# =============================================================================
+# Print completed level 2 task text box and increment level 2 task counter.
+#
+# Prints to the terminal a completed task message, including the task execution
+# duration, in a pre-formatted text box whose:
+# - Border is the "+" character
+# - Foreground text color is black.
+# - Background text color is bright yellow.
+#
+# @param $1 Message to be printed in the text box.
+# @param $2 Duration of the completed task.
+#
+# @return 0.
+# =============================================================================
+
+printCompletedL2TaskTextBoxAndIncrementCounter() {
+	local message=$1
+	local duration=$2
+
+	echo
+
+	# Store in the  `hhmmss` variable the task execution duration in the
+	# `hh:mm:ss` format.
+	printf -v hhmmss \
+		'%02dh:%02dm:%02ds' \
+		$((duration / 3600)) $((duration % 3600 / 60)) $((duration % 60))
+
+	# Print to the terminal the pre-formatted text box.
+	printNormalTextBox \
+		$FG_COLOR_BLACK \
+		$BG_COLOR_BRIGHT_YELLOW \
+		"+" \
+		"[$currentL2TaskNumber/$totalL2TaskCount] Completed $message in $hhmmss"
+	((currentL2TaskNumber++))
+
+	return 0
+}
+
+# =============================================================================
+# Execute L2 task.
+#
+# Prints to the terminal messages related to L2 task execution start and
+# completion, and execute the task.
+#
+# @param $1 Message to print in the staring / completed L2 task text boxes.
+# @param $2 Command corresponding to the task to be executed.
+#
+# @return 0.
+# =============================================================================
+
+executeL2Task() {
+	local message=$1
+	local command=$2
+
+	# Print staring L2 task text box and store start date.
+	printStartingL2TaskTextBox "$message"
+	currentLvlTwoTaskStartTimeStamp=$(date +%s)
+
+	# Print execute command message and execute the task
+	printExecutingCommand "$command"
+	eval $command
+
+	# Store end date and print completed L2 task text box.
+	currentLvlTwoTaskEndTimeStamp=$(date +%s)
+	printCompletedL2TaskTextBoxAndIncrementCounter \
+		"$message" \
+		"$((currentLvlTwoTaskEndTimeStamp - currentLvlTwoTaskStartTimeStamp))"
+
+	return 0
+}
+
+# =============================================================================
+# Print various messages.
+#
+# Prints to the terminal various messages shared across multiple Bash scripts.
+# =============================================================================
+
+printRemarksHeader() {
+	echo
+	echo 'REMARKS(S):'
+	return 0
+}
+
+printTaskFailureWarningsHeader() {
+	echo
+	echo 'WARNING(S): Above task(s) will fail if:'
+	return 0
+}
+
+printGuidanceHeader() {
+	echo
+	echo 'GUIDANCE:'
+	return 0
+}
+
+printWarningForFailureIfUnmetPrerequisites() {
+	echo -n '
+- The prerequisites are unmet.
+  -> Run the "01_InstallSoftwarePrerequisites.bash" script to make sure all are
+     met.'
+	echo
+	return 0
+}
+
+printWarningFailureIfProjectBinaryTreeNotBuilt() {
+	echo -n '
+- The project binary trees are not built under the 'bld' directory.
+  -> Run the "03_BuildProgramAndSourceCodeDoc.bash" script to do so.'
+	echo
+	return 0
+}
+
+printInfoMessageNextPossibleActions() {
+	echo -e '
+You can now:'
+	return 0
+}
+
+printInfoBrowseSourceCodeDocumentation() {
+	echo -n -e '
+- Browse the '$SET_STYLE_ITALIC'ReactorCodingChallenge01'$UNSET_STYLE_ALL' program source code documentation at:
+  '$FG_COLOR_CYAN$SET_STYLE_UNDERLINE'bld/01_default/SourceCodeDocumentation/html/index.html'$UNSET_STYLE_ALL'.'
+	echo
+	return 0
+}
+
+printInfoExecuteUnitTests() {
+	echo -n -e '
+- Execute unit tests for the '$SET_STYLE_ITALIC'ReactorCodingChallenge01'$UNSET_STYLE_ALL' program.
+  -> Run the "05_RunUnitTests.bash" script.'
+	echo
+	return 0
+}
+
+printInfoBrowseTestsSourceCodeCoverageReport() {
+	testType=$1
+	if [[ $testType == "Unit" ]]; then
+		configuration="02_coverage"
+	else
+		configuration="ToDo"
+	fi
+	echo -n -e '
+- Browse the '$testType' tests source code coverage report at:
+  '$FG_COLOR_CYAN$SET_STYLE_UNDERLINE'bld/'$configuration'/'$testType'TestsCoverageReport/html/index.html'$UNSET_STYLE_ALL'.'
+	echo
+	return 0
+}
+
+printInfoExecuteFuzzTestsInUnitTestMode() {
+	echo -n -e '
+- Execute fuzz tests in unit test mode for the '$SET_STYLE_ITALIC'ReactorCodingChallenge01'$UNSET_STYLE_ALL' program.
+  -> Run the "06_RunFuzzTestsInUnitTestMode.bash" script.'
+	echo
+	return 0
+}
+
+printInfoExecuteFuzzTestsInFuzzingMode() {
+	echo -n -e '
+- Execute fuzz tests in fuzzing mode for the '$SET_STYLE_ITALIC'ReactorCodingChallenge01'$UNSET_STYLE_ALL' program.
+  -> Run the "07_RunFuzzTestsInFuzzingMode.bash" script.'
+	echo
+	return 0
+}
+
+printInfoExecuteIntegrationTests() {
+	echo -n -e '
+- Execute integration tests for the '$SET_STYLE_ITALIC'ReactorCodingChallenge01'$UNSET_STYLE_ALL' program.
+  -> Run the "08_RunIntegrationTests.bash" script.'
+	echo
+	return 0
+}
+
+printInfoExecuteGitPreCommitHooks() {
+	echo -n -e '
+- Execute Git pre-commit hooks for the '$SET_STYLE_ITALIC'ReactorCodingChallenge01'$UNSET_STYLE_ALL' project.
+  -> Run the "09_RunGitPrecommitHooks.bash" script.'
+	echo
+	return 0
+}
+
+printExecutingCommand() {
+	local command=$1
+	echo "Executing the following command..."
+	echo
+	echo -e $FG_COLOR_BRIGHT_MAGENTA"$command"$UNSET_STYLE_ALL
+	echo
+	return 0
+}
