756de7f58d21c9a44dad8ee2d35e53e4215ab8bb
Fabien Launay <fabien.launay.email@gmail.com>
2024-10-20 21:37:39 +0900

    oppress 24/10/20 21:37:39 089943800
    Line 10
    
    Line 11

--- CreateBadCommitsOnMain.bash
+++ CreateBadCommitsOnMain.bash
@@ -1,27 +1,33 @@
 #!/bin/bash
 
+verbs="cfg/gitlint/txt/Verbs.txt"
+total_lines=$(wc -l < "$verbs")
+random_line_number=$((RANDOM % total_lines + 1))
+verb=$(sed -n "${random_line_number}p" "$verbs")
+
 d=$(date "+%y/%m/%d %T %N")
-echo "$d" > README.md
+echo "$verb $d" > README.md
 
 git add .
 git commit \
-  -m "FEA1: Update README.md with $d" \
+  -m "$verb $d" \
   -m "Line 10" \
   -m "Line 11"
 
 d=$(date "+%y/%m/%d %T %N")
-echo "$d" > README.md
+echo "$verb $d" > README.md
 git add .
 git commit \
-  -m "FEA2: Update README.md with $d" \
+  -m "$verb $d" \
   -m "Line 20" \
   -m "Line 21"
 
-  d=$(date "+%y/%m/%d %T %N")
-  echo "$d" > README.md
-  git add .
-  git commit \
-    -m "FEA3: Update README.md with $d" \
+d=$(date "+%y/%m/%d %T %N")
+echo "$verb $d" > README.md
+git add .
+git commit \
+    -m "$verb $d" \
     -m "Line 30" \
     -m "Line 31"
+
 git push
