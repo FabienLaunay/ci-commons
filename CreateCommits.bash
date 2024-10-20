cff03cde0d29da59c1a639e337e9e3014bbc3e67
Fabien Launay <fabien.launay.email@gmail.com>
2024-10-20 13:56:58 +0900

    FEA1: Update README.md

--- CreateCommits.bash
+++ CreateCommits.bash
@@ -2,14 +2,14 @@
 
 date "+%y/%m/%d %T %N" > README.md
 git add .
-git commit -m 'FEA1: Update "+%y/%m/%d %T %N" README.md'
+git commit -m 'FEA1: Update README.md'
 
 date "+%y/%m/%d %T %N" > README.md
 git add .
-git commit -m 'FEA2: Update "+%y/%m/%d %T %N" README.md'
+git commit -m 'FEA2: Update README.md'
 
 date "+%y/%m/%d %T %N" > README.md
 git add .
-git commit -m 'FEA: Update "+%y/%m/%d %T %N" 3 README.md'
+git commit -m 'FEA: Update Update README.md'
 
-git push
+#git push
