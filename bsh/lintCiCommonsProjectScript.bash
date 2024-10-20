ae2cafbadf0a6db46eede4714126f5cffe1ecb36
Fabien Launay <fabien.launay.email@gmail.com>
2024-10-20 17:46:13 +0900

    FEA1: Update README.md

--- bsh/lintCiCommonsProjectScript.bash
+++ bsh/lintCiCommonsProjectScript.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 . bsh/utilities/message.bash
-. bsh/utilities/strings.bash
+. bsh/utilities/actions.bash
 
 currentL1TaskNumber=4
-totalL1TaskCount=4
+totalL1TaskCount=5
 
 # ==============================================================================
 # Run Gitlint on every commit between the provided range
@@ -20,7 +20,10 @@ commits=$(git rev-list $1..$2)
 currentL2TaskNumber=1
 totalL2TaskCount=$(echo "$commits" | wc -l)
 
-gitlintFailed=false
+lintCount=0
+failureCount=0
+modifyFrom=-1
+actions=()
 
 for commit in $commits; do
   # Print starting text box level 2.
@@ -32,11 +35,18 @@ for commit in $commits; do
   command="gitlint --config cfg/gitlint/gitlint.cfg --commit $commit"
   printExecutingCommand "$command"
   $command
-#  RESULT=$command
+
   if [[ $? -ne 0 ]]; then
-      echo "RESULT=$RESULT"
-      gitlintFailed=true  # Set flag if gitlint fails
+      if [[ $modifyFrom -eq -1 ]]; then
+        modifyFrom=$(($totalL2TaskCount-$lintCount))
+      fi
+      ((failureCount++))
+      actions+=("reword $COMMIT $COMMIT_MESSAGE")
+    else
+      actions+=("pick $COMMIT $COMMIT_MESSAGE")
   fi
+
+  ((lintCount++))
 
   # Print completed text box level 2.
   currentLvlTwoTaskEndTimeStamp=$(date +%s)
@@ -44,12 +54,42 @@ for commit in $commits; do
   "$((currentLvlTwoTaskEndTimeStamp - currentLvlOneTaskStartTimeStamp))"
 done
 
+# Print completed text box level 1.
+currentLvlOneTaskEndTimeStamp=$(date +%s)
+printCompletedL1TaskTextBoxAndIncrementCounter "$message" \
+  "$((currentLvlOneTaskEndTimeStamp - currentLvlOneTaskStartTimeStamp))"
 
-  if [[ "$gitlintFailed" == true ]]; then
-    printGuidanceHeader
-    echo "One or more gitlint commands failed."
+# ==============================================================================
+# Run Gitlint on every commit between the provided range
+# ==============================================================================
+
+# Print starting text box level 1.
+message="Gitlint report generation"
+printStartingL1TaskTextBox "$message"
+currentLvlOneTaskStartTimeStamp=$(date +%s)
+
+  if [[ $failureCount -eq 0 ]]; then
+    if [[ $totalL2TaskCount -eq 1 ]]; then
+      echo "Git commit message is valid."
+    else
+      echo "All of $totalL2TaskCount Git commit messages are valid."
+    fi
+  else
+    if [[ $totalL2TaskCount -eq 1 ]]; then
+      echo "Git commit message is not valid."
+    else
+      echo "failureCount out of $totalL2TaskCount Git commit messages are not valid."
+    fi
+    echo "1.Run the following command to open the default Git text editor:"
+    echo "  $ git rebase --interactive HEAD~$modifyFrom"
+    echo $NEW_LINE
+    echo "2.Replace the content of the editor by the following one:"
+    echo "###################################"
+    for action in "${actions[@]}"; do
+      echo "$action"
+    done    
+    echo "###################################"
   fi
-
 
 # Print completed text box level 1.
 currentLvlOneTaskEndTimeStamp=$(date +%s)
